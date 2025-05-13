41ea3f63d541bb7ed6a12ea2cd41e359f97f033f
Kishan Prajapati <kmprajapati1910@gmail.com>
2025-05-13 13:02:56 +0530

    Vercel and render deployment done

--- client/src/utils/axiosConfig.js
+++ client/src/utils/axiosConfig.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 // Create a custom axios instance
 const axiosInstance = axios.create({
-  // baseURL: 'http://localhost:3000/api', // Ensure this is set correctly
-  baseURL: import.meta.env.VITE_BASE_URL, // Ensure this is set correctly
+  // baseURL: 'http://localhost:3000/api', 
+  baseURL: import.meta.env.VITE_BASE_URL, 
 });
 
 // Add a request interceptor to set the Authorization header
